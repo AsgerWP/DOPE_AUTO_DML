2e5b5c7544e8b81dac513069c695b013c7383fe2
AsgerWP <lzt134@alumni.ku.dk>
2026-04-20 14:43:11 +0200

    feat: add load_replication method

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class ATEDataset:
     def __init__(self, data, treatment_column, outcome_column, covariate_columns):
         self.data = data
@@ -9,3 +12,9 @@ class ATEDataset:
 class IHDPDataset(ATEDataset):
     def __init__(self, data, treatment_column, outcome_column, covariate_columns):
         super().__init__(data, treatment_column, outcome_column, covariate_columns)
+
+    @classmethod
+    def load_replication(cls, replication_id):
+        path = 'datasets/ihdp_replications/ihdp_' + str(replication_id) + '.csv'
+        data = np.loadtxt(path)
+        return cls(data=data, treatment_column=0, outcome_column=1, covariate_columns=[i + 5 for i in range(25)])
