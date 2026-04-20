a632c24fe3d2e911a2c9e4b77a4c1e78da1fff48
AsgerWP <lzt134@alumni.ku.dk>
2026-04-20 14:39:09 +0200

    feat: add class IHDPDataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -4,3 +4,8 @@ class ATEDataset:
         self.treatment_column = treatment_column
         self.outcome_column = outcome_column
         self.covariate_columns = covariate_columns
+
+
+class IHDPDataset(ATEDataset):
+    def __init__(self, data, treatment_column, outcome_column, covariate_columns):
+        super().__init__(data, treatment_column, outcome_column, covariate_columns)
