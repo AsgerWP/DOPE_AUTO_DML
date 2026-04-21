2b33d0b77f3ce80d74444fd4aada8d054b0ef08a
AsgerWP <lzt134@alumni.ku.dk>
2026-04-21 22:31:23 +0200

    feat: create THead, SHead

--- models/dope_neural_net.py
+++ models/dope_neural_net.py
@@ -1,1 +1,42 @@
-class
+import torch
+from torch import nn
+
+from models.utils import MLP
+
+
+class THead(nn.Module):
+    def __init__(self, representation_size, hidden_sizes, activation):
+        super().__init__()
+        self.t_layers = MLP(
+            input_size=representation_size,
+            hidden_sizes=hidden_sizes,
+            output_size=1,
+            activation=activation,
+            activation_after_final_layer=False,
+        )
+        self.c_layers = MLP(
+            input_size=representation_size,
+            hidden_sizes=hidden_sizes,
+            output_size=1,
+            activation=activation,
+            activation_after_final_layer=False,
+        )
+
+    def forward(self, representation, treatment):
+        return self.t_layers(representation) * treatment + self.c_layers(representation) * (1 - treatment)
+
+
+class SHead(nn.Module):
+    def __init__(self, representation_size, hidden_sizes, activation):
+        super().__init__()
+        self.layers = MLP(
+            input_size=representation_size + 1,
+            hidden_sizes=hidden_sizes,
+            output_size=1,
+            activation=activation,
+            activation_after_final_layer=False,
+        )
+
+    def forward(self, representation, treatment):
+        x = torch.cat([representation, treatment], dim=1)
+        return self.layers(x)
