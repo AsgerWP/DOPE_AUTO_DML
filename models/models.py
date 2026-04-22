c5168d34ab61bcc42197c83cc60968a171d2b2e9
AsgerWP <lzt134@alumni.ku.dk>
2026-04-22 14:32:02 +0200

    refactor: rename variables

--- models/models.py
+++ models/models.py
@@ -25,11 +25,11 @@ class DOPENeuralNet(nn.Module):
             activation_after_final_shared_layer,
         )
         if branch_type == "T":
-            self.outcome_head = THead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
-            self.riesz_head = THead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
+            self.outcome_branch = THead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
+            self.riesz_branch = THead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
         elif branch_type == "S":
-            self.outcome_head = SHead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
-            self.riesz_head = SHead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
+            self.outcome_branch = SHead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
+            self.riesz_branch = SHead(shared_hidden_layers[-1], not_shared_hidden_layers, activation, dropout_prob)
         else:
             raise ValueError("Invalid branch type. Must be 'T' or 'S'.")
 
@@ -40,15 +40,15 @@ class DOPENeuralNet(nn.Module):
     def get_outcome_mse_loss(self, batch):
         covariates, treatment, outcome = batch
         representation = self.shared_trunk(covariates)
-        outcome_prediction = self.outcome_head(representation, treatment)
+        outcome_prediction = self.outcome_branch(representation, treatment)
         return nn.functional.mse_loss(outcome_prediction, outcome)
 
     def get_riesz_loss(self, batch):
         covariates, treatment, _ = batch
         representation = self.shared_trunk(covariates)
-        riesz_prediction = self.riesz_head(representation, treatment)
-        riesz_treatment = self.riesz_head(representation, torch.ones_like(treatment))
-        riesz_control = self.riesz_head(representation, torch.zeros_like(treatment))
+        riesz_prediction = self.riesz_branch(representation, treatment)
+        riesz_treatment = self.riesz_branch(representation, torch.ones_like(treatment))
+        riesz_control = self.riesz_branch(representation, torch.zeros_like(treatment))
         return (riesz_prediction**2 - 2 * (riesz_treatment - riesz_control)).mean()
 
     def get_estimates(self, data):
@@ -57,10 +57,10 @@ class DOPENeuralNet(nn.Module):
         treatment = data.treatments_tensor().to(device)
         outcome = data.outcomes_tensor().to(device)
         representation = self.shared_trunk(covariates)
-        outcome_prediction = self.outcome_head(representation, treatment)
-        riesz_prediction = self.riesz_head(representation, treatment)
-        treated_outcome_prediction = self.outcome_head(representation, torch.ones_like(treatment))
-        control_outcome_prediction = self.outcome_head(representation, torch.zeros_like(treatment))
+        outcome_prediction = self.outcome_branch(representation, treatment)
+        riesz_prediction = self.riesz_branch(representation, treatment)
+        treated_outcome_prediction = self.outcome_branch(representation, torch.ones_like(treatment))
+        control_outcome_prediction = self.outcome_branch(representation, torch.zeros_like(treatment))
         plugin_terms = treated_outcome_prediction - control_outcome_prediction
         correction_terms = riesz_prediction * (outcome - outcome_prediction)
         dr_terms = plugin_terms + correction_terms
