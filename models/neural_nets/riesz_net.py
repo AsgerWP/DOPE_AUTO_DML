33513dfa7a7cd377f5ac3f12ff881e114bb10f70
AsgerWP <lzt134@alumni.ku.dk>
2026-04-23 17:44:02 +0200

    refactor: use NeuralNet ABC

--- models/neural_nets/riesz_net.py
+++ models/neural_nets/riesz_net.py
@@ -3,10 +3,11 @@ import copy
 import torch
 from torch import nn
 
+from models.neural_nets.neural_net import NeuralNetwork
 from models.neural_nets.utils import MLP, TBranch, SBranch
 
 
-class RieszNet(nn.Module):
+class RieszNet(NeuralNetwork):
     def __init__(
         self,
         moment_functional,
@@ -18,8 +19,7 @@ class RieszNet(nn.Module):
         loss_weights,
         dropout_prob=0,
     ):
-        super().__init__()
-        self.moment_functional = moment_functional
+        super().__init__(moment_functional)
         self.shared_trunk = MLP(
             input_size=n_covariates + 1,
             hidden_sizes=shared_hidden_layers[:-1],
@@ -64,7 +64,9 @@ class RieszNet(nn.Module):
         return self.riesz_branch(self.shared_trunk(net_input.to(device)))
 
     def outcome_forward(self, covariates, treatment):
-        return self.uncorrected_outcome_forward(covariates, treatment) + self.epsilon * self.riesz_forward(covariates, treatment)
+        return self.uncorrected_outcome_forward(covariates, treatment) + self.epsilon * self.riesz_forward(
+            covariates, treatment
+        )
 
     def get_riesz_net_loss(self, batch):
         covariates, treatment, outcome = batch
@@ -82,76 +84,15 @@ class RieszNet(nn.Module):
             + self.loss_weights["tmle"] * tmle_loss
         )
 
-    def get_estimates(self, data):
-        covariates = data.covariates_tensor()
-        treatment = data.treatments_tensor()
-        outcome = data.outcomes_tensor()
-
-        plugin_terms = self.moment_functional(self.outcome_forward, covariates, treatment)
-        correction_terms = self.riesz_forward(covariates, treatment) * (
-            outcome - self.outcome_forward(covariates, treatment)
-        )
-        dr_terms = plugin_terms + correction_terms
-        return {"point_estimate": dr_terms.mean().item(), "var_estimate": dr_terms.var().item()}
-
-    def fit(self, data, lr, weight_decay, batch_size, epochs, patience):
-        decay_params = []
-        no_decay_params = []
-
-        for name, param in self.named_parameters():
-            if not param.requires_grad:
-                continue
-            if name == "epsilon":
-                no_decay_params.append(param)
-            else:
-                decay_params.append(param)
-
-        optimizer = torch.optim.Adam(
-            [
-                {"params": decay_params, "weight_decay": weight_decay},
-                {"params": no_decay_params, "weight_decay": 0.0},
-            ],
+    def fit(self, data, lr, weight_decay, batch_size, epochs, patience, lambda_lasso=0):
+        self._fit(
+            data=data,
+            loss_fn=self.get_riesz_net_loss,
+            trunk=self.shared_trunk,
             lr=lr,
+            weight_decay=weight_decay,
+            batch_size=batch_size,
+            epochs=epochs,
+            patience=patience,
+            lambda_lasso=lambda_lasso,
         )
-
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            mode="min",
-            factor=0.5,
-            patience=10,
-            threshold=1e-3,
-            threshold_mode="rel",
-            cooldown=2,
-            min_lr=1e-6,
-        )
-        train_data, val_data = data.split_into_train_and_validation_sets(train_size=0.8)
-        train_loader = train_data.create_dataloader(batch_size=batch_size)
-        best = 1e6
-        counter = 0
-        best_state = copy.deepcopy(self.state_dict())
-        for epoch in range(epochs):
-            self.train()
-            for batch in train_loader:
-                optimizer.zero_grad()
-                loss = self.get_riesz_net_loss(batch)
-                loss.backward()
-                optimizer.step()
-            self.eval()
-            with torch.no_grad():
-                val_loss = self.get_riesz_net_loss(
-                    (
-                        val_data.covariates_tensor(),
-                        val_data.treatments_tensor(),
-                        val_data.outcomes_tensor(),
-                    )
-                )
-                scheduler.step(val_loss)
-                if val_loss.item() < best:
-                    best = val_loss.item()
-                    counter = 0
-                    best_state = copy.deepcopy(self.state_dict())
-                else:
-                    counter += 1
-                    if counter == patience:
-                        self.load_state_dict(best_state)
-                        break
